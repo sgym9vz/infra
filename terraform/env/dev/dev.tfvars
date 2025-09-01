2fbdc248a9fdcf54a4b1569588c402a93c51ca9a
sgym9vz <97873596+sgym9vz@users.noreply.github.com>
2025-09-01 12:36:07 +0900

    vpc update

--- terraform/env/dev/dev.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region  = "ap-northeast-1"
-name        = "dev-vpc"
-cidr        = "10.10.0.0/16"
-environment = "dev"
--- terraform/env/dev/providers.tf
+++ terraform/env/dev/providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-#  backend "s3" {}  # values are passed from CLI in the workflow
-}
-
 provider "aws" {
   region = var.aws_region
 }
