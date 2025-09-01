909215fe7a2b37be1955a738857e4fc3d9f1fac3
sgym9vz <97873596+sgym9vz@users.noreply.github.com>
2025-09-02 02:39:51 +0900

    fix

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -35,11 +35,16 @@ resource "aws_security_group" "app_sg" {
 # EC2 Instance
 resource "aws_instance" "app" {
   ami                         = var.ami_id
-  instance_type               = "t3.micro"
+  instance_type               = "t3.small"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_name
+
+  root_block_device {
+    volume_size = 30      # GiB
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "${var.name}-ec2"
